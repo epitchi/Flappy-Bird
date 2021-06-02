2d4f9c600dad0c355a23e80153662c56c17d85c0
epitchi <thienvanlea1@gmail.com>
2021-06-02 00:29:33 -0700

    Handle collisions with rect

--- main.py
+++ main.py
@@ -23,14 +23,24 @@ def draw_pipes(pipes):
 			flip_pipe = pygame.transform.flip(pipe_surface, False, True)
 			screen.blit(flip_pipe, pipe)
 
+def check_collision(pipes):
+	for pipe in pipes:
+		if bird_rect.colliderect(pipe):
+			return False
+
+		if bird_rect.top <= -100 or bird_rect.bottom >= 900:
+			return False
+
+	return True
+
 pygame.init()
 screen = pygame.display.set_mode((576, 1024))
 clock = pygame.time.Clock()
 
 #Game Variables
-gravity = 0.25
+gravity = 0.20
 bird_movement = 0
-
+game_active = True
 
 bg_surface = pygame.transform.scale2x(pygame.image.load('assets/background-day.png').convert())
 
@@ -57,24 +67,32 @@ while True:
 			pygame.quit()
 			sys.exit()
 		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_SPACE:
+			if event.key == pygame.K_SPACE and game_active:
 				bird_movement = 0 
 				bird_movement -=9
+			if event.key == pygame.K_SPACE and game_active == False:
+				game_active = True
+				pipe_list.clear()
+				bird_rect.center = (100, 512)
+				bird_movement = 0
+
+
 		if event.type == SPAWNPIPE:
 			pipe_list.extend(create_pipe())				
 
 
 	screen.blit(bg_surface,(0,0)) #put surface
 
-	#Bird
-	bird_movement += gravity
-	bird_rect.centery += bird_movement
-	screen.blit(bird_surface, bird_rect)
+	if game_active:
+		#Bird
+		bird_movement += gravity
+		bird_rect.centery += bird_movement
+		screen.blit(bird_surface, bird_rect)
+		game_active = check_collision(pipe_list)
 
-
-	#Pipes
-	pipe_list = move_pipes(pipe_list)
-	draw_pipes(pipe_list)
+		#Pipes
+		pipe_list = move_pipes(pipe_list)
+		draw_pipes(pipe_list)
 
 
 
