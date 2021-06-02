f28eb8b779ffcc8b4583cb44b166767beaa77f46
epitchi <thienvanlea1@gmail.com>
2021-06-02 12:02:54 -0700

    Adding the score as text

--- main.py
+++ main.py
@@ -41,14 +41,41 @@ def bird_animation():
 	new_bird = bird_frames[bird_index]
 	new_bird_rect = new_bird.get_rect(center = (100, bird_rect.centery))
 	return new_bird, new_bird_rect
+
+def score_display(game_state):
+	if game_state == 'main_game':
+		score_surface = game_font.render(str(int(score)), True, (255, 255, 255))
+		score_rect = score_surface.get_rect(center = (288, 100))
+		screen.blit(score_surface, score_rect)
+	if game_state == 'game_over':
+		score_surface = game_font.render(f'Score: {int(score)}', True, (255, 255, 255))
+		score_rect = score_surface.get_rect(center = (288, 100))
+		screen.blit(score_surface, score_rect)
+
+		high_score_surface = game_font.render(f'High score: {int(high_score)}' , True, (255, 255, 255))
+		high_score_rect = score_surface.get_rect(center = (240, 850))
+		screen.blit(high_score_surface, high_score_rect)
+
+def update_score(score, high_score):
+	if score > high_score:
+		high_score = score
+	return high_score
+
+
 pygame.init()
 screen = pygame.display.set_mode((576, 1024))
 clock = pygame.time.Clock()
+game_font = pygame.font.Font('04B_19.ttf',40)
 
 #Game Variables
 gravity = 0.20
 bird_movement = 0
 game_active = True
+score = 0
+high_score = 0
+
+
+
 
 bg_surface = pygame.transform.scale2x(pygame.image.load('assets/background-day.png').convert())
 
@@ -87,12 +114,13 @@ while True:
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_SPACE and game_active:
 				bird_movement = 0 
-				bird_movement -=9
+				bird_movement -=8.5
 			if event.key == pygame.K_SPACE and game_active == False:
 				game_active = True
 				pipe_list.clear()
 				bird_rect.center = (100, 512)
 				bird_movement = 0
+				score = 0
 
 		if event.type == SPAWNPIPE:
 			pipe_list.extend(create_pipe())				
@@ -118,6 +146,12 @@ while True:
 		pipe_list = move_pipes(pipe_list)
 		draw_pipes(pipe_list)
 
+		score +=0.01
+
+		score_display('main_game')
+	else:
+		high_score = update_score(score, high_score)
+		score_display('game_over')
 
 
 	#Floor
